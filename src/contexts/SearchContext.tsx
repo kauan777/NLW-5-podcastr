c20c79c2595b6b14eace28866c661511261e26ca
kauan777 <kauan7622@gmail.com>
2023-01-07 19:42:50 -0300

    refactor: changing name of the state

--- src/contexts/SearchContext.tsx
+++ src/contexts/SearchContext.tsx
@@ -1,30 +1,31 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 
-
 type SearchContextProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
 type SearchContextData = {
-    podcast: string;
-    setPodcast: (podcast: string) => void
+  titlePodcast: string;
+  setTitlePodcast: (podcast: string) => void;
+};
+
+export const SearchContext = createContext({} as SearchContextData);
+
+export function SearchContextProvider({
+  children,
+}: SearchContextProviderProps) {
+  const [titlePodcast, setTitlePodcast] = useState("");
+
+  return (
+    <SearchContext.Provider
+      value={{
+        titlePodcast,
+        setTitlePodcast,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
 }
 
-export const SearchContext = createContext({} as SearchContextData);    
-
-export function SearchContextProvider({children}: SearchContextProviderProps){
-
-    const [podcast, setPodcast] = useState("")  
-    
-    return (
-        <SearchContext.Provider
-            value={{
-                podcast,
-                setPodcast
-            }}>
-            {children}
-        </SearchContext.Provider>
-        )
-}
-
-export const useSearch = () => useContext(SearchContext)
+export const useSearch = () => useContext(SearchContext);
