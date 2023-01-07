efd5cfee100658780812eb50299e0d9389d3b88f
kauan777 <kauan7622@gmail.com>
2023-01-07 19:44:41 -0300

    refactor: changing props of the useSearch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,45 +1,36 @@
-import format from 'date-fns/format'
-import ptBr from 'date-fns/locale/pt-BR';
-import { ImSearch } from 'react-icons/im'
-import { useRouter } from 'next/router'
+import format from "date-fns/format";
+import ptBr from "date-fns/locale/pt-BR";
+import { ImSearch } from "react-icons/im";
 
-import styles from './styles.module.scss';
-import { useSearch } from '../../contexts/SearchContext';
-
+import styles from "./styles.module.scss";
+import { useSearch } from "../../contexts/SearchContext";
 
 //date fns - lida com data no React
 
-
-export function Header(){
-  
-  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-    locale: ptBr 
+export function Header() {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBr,
   });
 
-  const router = useRouter()
-  const {podcast, setPodcast} = useSearch()
+  const { titlePodcast, setTitlePodcast } = useSearch();
 
-  console.log(podcast)
-
-    return(
-      <header className={styles.headerContainer}>
-          <img src="/logo.svg" alt="Podcastr"/>
-          <div className={styles.containerInput}>
-            <input 
-              type="text" 
-              placeholder='Pesquise um podcast...' 
-              onChange={(e) => {
-                e.target.value.length > 0 ? router.push("/search") : router.push("/")
-                setPodcast(e.target.value)
-              }}
-              value={podcast}
-            />
-            <button>
-              <ImSearch color='#fff' className={styles.searchIcon}/>
-            </button>
-          </div>
-          <span>{currentDate}</span>
-      </header>
-
-    );
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+      <div className={styles.containerInput}>
+        <input
+          type="text"
+          placeholder="Pesquise um podcast..."
+          onChange={(e) => {
+            setTitlePodcast(e.target.value);
+          }}
+          value={titlePodcast}
+        />
+        <button>
+          <ImSearch color="#fff" className={styles.searchIcon} />
+        </button>
+      </div>
+      <span>{currentDate}</span>
+    </header>
+  );
 }
